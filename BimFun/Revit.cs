b197c13473d006774f3564197c90a6d44bd04fd1
Young <iamlovedit@163.com>
2020-01-04 23:46:58 +0800

    add new modules

--- BimFun/Revit.cs
+++ BimFun/Revit.cs
@@ -15,6 +15,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.DB.Structure;
 
 namespace BimFun
 {
@@ -119,7 +120,6 @@ namespace BimFun
                 throw new Exception(e.Message);
             }
         }
-
         /// <summary>
         /// 获取revit中指定类型的族
         /// </summary>
@@ -130,6 +130,43 @@ namespace BimFun
             return new FilteredElementCollector(doc).OfClass(typeof(FamilySymbol)).OfCategoryId(new ElementId(category.Id)).Cast<FamilySymbol>()
                   .ToList().OrderBy(x => x.Name, new FileComparer()).ToList().ConvertAll(x => ElementWrapper.Wrap(x as FamilySymbol, false));
 
+        }
+
+        /// <summary>
+        /// 通过xyz值放置族实例
+        /// </summary>
+        /// <param name="familyType">用于放置的族</param>
+        /// <param name="x">X坐标值</param>
+        /// <param name="y">Y坐标值</param>
+        /// <param name="z">Z坐标值</param>
+        /// <returns>放置好的族实例</returns>
+        public static Revit.Elements.FamilyInstance FamilyinstanceByXYZ(Revit.Elements.FamilyType familyType, double x, double y, double z)
+        {
+            TransactionManager.Instance.EnsureInTransaction(doc);
+            XYZ point = new XYZ(x, y, z);
+            FamilySymbol familySymbol = (FamilySymbol)familyType.InternalElement;
+            if (!familySymbol.IsActive) familySymbol.Activate();
+            Autodesk.Revit.DB.FamilyInstance familyInstance = doc.Create.NewFamilyInstance(point, familySymbol, Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
+            return familyInstance.ToDSType(true) as Revit.Elements.FamilyInstance;
+        }
+        /// <summary>
+        /// 通过xyz和标高放置族实例
+        /// </summary>
+        /// <param name="familyType">族类型</param>
+        /// <param name="x">x值</param>
+        /// <param name="y">y值</param>
+        /// <param name="z">z值</param>
+        /// <param name="level">标高</param>
+        /// <returns></returns>
+        public static Revit.Elements.FamilyInstance FamilyinstanceByXYZ(Revit.Elements.FamilyType familyType, double x, double y, double z, Revit.Elements.Level level)
+        {
+            TransactionManager.Instance.EnsureInTransaction(doc);
+            XYZ point = new XYZ(x, y, z);
+            FamilySymbol familySymbol = (FamilySymbol)familyType.InternalElement;
+            if (!familySymbol.IsActive) familySymbol.Activate();
+            Autodesk.Revit.DB.FamilyInstance familyInstance = doc.Create.NewFamilyInstance(point, familySymbol,(Autodesk.Revit.DB.Level)level.InternalElement, 
+                Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
+            return familyInstance.ToDSType(true) as Revit.Elements.FamilyInstance;
         }
     }
 }
